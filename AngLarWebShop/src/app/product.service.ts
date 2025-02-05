332b3c4162e47e1bf1efc2304924759a7d5fadbb
vamosilaszlo <vamosilaszlo@outlook.com>
2025-02-05 10:15:31 +0100

    'product.service.ts_laravel_api_products'

--- AngLarWebShop/src/app/product.service.ts
+++ AngLarWebShop/src/app/product.service.ts
@@ -7,28 +7,13 @@ import { Observable, of } from 'rxjs';
 })
 
 export class ProductService {
-  private apiUrl = 'https://fakestoreapi.com/products';  //?limit=10
+  // private apiUrl = 'https://fakestoreapi.com/products';  //?limit=10
+  private apiUrl = 'http://localhost:8000/api/products';  //?limit=10
 
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<any> {
     return this.http.get<any>(this.apiUrl);
   }
-
-
-
-
-  // getProducts(): Observable<any[]> {
-
-  //   return of([
-  //     { title: 'Smartphone', description: 'A fast smartphone', price: 299, image: 'https://via.placeholder.com/150' },
-  //     { title: 'Laptop', description: 'High performance laptop', price: 999, image: 'https://via.placeholder.com/150' },
-  //     { title: 'Headphones', description: 'Noise-cancelling headphones', price: 199, image: 'https://via.placeholder.com/150' },
-  //   ]);
-  // }
-
-
-
-
 
 }
